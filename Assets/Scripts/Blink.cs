ce855c8658d8f4d0cb395727871bcaf812c20290
Renan Brando <reebrando@gmail.com>
2018-02-07 22:41:59 -0200

    Added project.

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Blink : MonoBehaviour {
+
+	// Interval to turn on/off rendering 
+	public float interval;
+
+	IEnumerator Start () {
+
+		// Gets object's component 
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (interval);
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (interval);
+
+		// Loops start method
+		StartCoroutine(Start());
+	}
+
+	void update (){
+		// Press enter
+		if(Input.GetKeyDown(KeyCode.Return)){
+			SceneManager.LoadScene ("game-scene");
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	public float impulse;
+
+	public Transform floorVerifier;
+	bool isOnFloor;
+
+	Animator anim;
+	SpriteRenderer spriteRenderer;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+		// Interface for components
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Move
+		float move_x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+		transform.Translate (move_x, 0.0f, 0.0f);
+		anim.SetFloat ("pHorizontal", Mathf.Abs (Input.GetAxisRaw ("Horizontal")));
+
+		// Verify colision with floor
+		isOnFloor = Physics2D.Linecast(transform.position, floorVerifier.position, 1 << LayerMask.NameToLayer("Floor"));
+
+		// Jump
+		if (Input.GetButtonDown ("Jump") && isOnFloor) {
+			rb.velocity = new Vector2 (0.0f, impulse);
+		}
+
+		// Orientation
+		if (move_x > 0) {
+			spriteRenderer.flipX = false;
+		} else if (move_x < 0) {
+			spriteRenderer.flipX = true;
+		}
+
+	}
+}
